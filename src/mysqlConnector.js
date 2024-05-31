2abc36d97e040bdd734f4db7ef365f2cf2ed91e5
Alex <yuanataalex@gmail.com>
2024-05-31 17:19:28 +0700

    finished gateway API

---
+++ src/mysqlConnector.js
@@ -0,0 +1,36 @@
+const mysql = require('mysql2');
+
+const connectionInfo = {
+  host: process.env.MYSQLHOST || '10.184.0.4',
+  port: process.env.MYSQLPORT || '3306',
+  user: process.env.MYSQLUSER || 'username', //these will always be wrong
+  password: process.env.MYSQLPASSWORD || 'password', //these will always be wrong
+  database: process.env.MYSQLDB || 'dataset',
+};
+
+console.log('connecting to database...');
+const connection = mysql.createConnection(connectionInfo);
+
+async function executeQuery(query) {
+   //connect to the database
+  connection.connect((error) => {
+    if (error){
+      console.log(err.message);
+      throw error;
+    }
+
+    console.log('Connected to the remote database!');
+  });
+
+  //if all is good, execute the query and return the result
+  connection.query(query, (err, results) => {
+    if (err){
+      console.log(err.message);
+      throw err;
+    }
+
+    return results;
+  });
+}
+
+module.exports = { executeQuery };
--- src/server.js
+++ src/server.js
@@ -1,16 +1,69 @@
 const express = require('express');
-const { testFetch } = require('./app');
+const { executeQuery } = require('./mysqlConnector');
 const app = express();
 
+//define a handler for queries
+async function handleQuery(req, res){
+  //get the query from payload
+  const payload = req.body;
+  const query = payload.query;
 
+  if (query == undefined){
+    return res.status(400).json({
+      status: 'failure',
+      message: 'no query detected in request body',
+    });
+  }
+
+  //retrieve result asynchronously
+  try {
+    const result = await executeQuery(query);
+
+    return res.status(200).json({
+      status: 'success',
+      message: 'query successfully retrieved',
+      data: result,
+    });
+  }
+  catch(err){
+    //something went wrong
+    return res.status(500).json({
+      status: 'failure',
+      message: 'something went wrong with the query',
+    });
+  }
+  
+
+}
+
+//server parses json payload
+app.use(express.json());
+
+//define an endpoint for queries
 app.get('/', (req, res) => {
-  const name = process.env.NAME || 'World';
-  res.send(`Hello ${name}!`);
+  return res.status(200).json({
+    status: 'success',
+    message: 'server is up and running!',
+  });
 });
 
+//define an endpoint for queries
+app.get('/query', handleQuery);
+
+//server listening to requests now
 const port = parseInt(process.env.PORT) || 8080;
-app.listen(port, () => {
-  console.log(`helloworld: listening on port ${port}`);
-});
 
-testFetch();
+async function startServer(){
+  //wait until we finish connecting to the database
+  try {
+    app.listen(port, () => {
+      console.log(`helloworld: listening on port ${port}`);
+    });
+  }
+  catch(err){
+    console.log(err.message);
+  }
+}
+
+//start the server
+startServer();
